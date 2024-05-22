decfb460b80b380918cd8a61d9c6c362111b8924
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-22 23:16:47 +0100

    installed antd switch

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios'
 import refresh from "./icon-refresh.svg"
+import { Switch } from 'antd';
 
 function App() {
 
-  const [time, setTime] = useState("1");
+  const [time, setTime] = useState("");
   const [quotes, setQuotes] = useState()
-  
+  const [abbrev, setAbbrev] = useState("")
+  const [timeZone, setTimezone] = useState("")
+
   useEffect(() => {
+
     const fetchQuotes = async () => {
       await axios.get("https://api.quotable.io/random?tags=technology&minLength=100&maxLength=180")
         .then(response => {
@@ -24,7 +28,12 @@ function App() {
       await axios.get("http://worldtimeapi.org/api/ip")
         .then(response => {
           console.log(response.data)
-          setTime(response.data.datetime)
+          const currentTime = new Date(response.data.datetime).toLocaleTimeString()
+          const abbreviation = response.data.abbreviation
+          const tz = response.data.timezone
+          setTime(currentTime)
+          setAbbrev(abbreviation)
+          setTimezone(tz)
         })
     }
     fetchTime()
@@ -36,7 +45,13 @@ function App() {
         <img src={refresh} alt='refresh icon' className='refresh-icon' />
       </div>
       <h2>{quotes}</h2>
-      {new Date(time).toLocaleString()}
+      <div className="time-div">
+        <h1>It is currently: {time}</h1>
+        <p>{abbrev}</p>
+        <p>{timeZone}</p>
+      </div>
+      <Switch checkedChildren="MORE" unCheckedChildren="LESS" />
+    <br />
     </div>
   );
 }
