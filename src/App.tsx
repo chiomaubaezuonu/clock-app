b010f57de393017860ee66b1055dfa3686a55a6c
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-22 22:33:31 +0100

    Added refresh icon for refreshing quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
+import refresh from "./icon-refresh.svg"
 
 function App() {
+
+  const [time, setTime] = useState("1");
+  const [quotes, setQuotes] = useState()
+
+  useEffect(() => {
+    const fetchTime = async () => {
+      await axios.get("http://worldtimeapi.org/api/ip")
+        .then(response => {
+          console.log(response.data)
+          setTime(response.data.datetime)
+        })
+    }
+    fetchTime()
+  }, [])
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      await axios.get("https://api.quotable.io/random?tags=technology&minLength=100&maxLength=180")
+        .then(response => {
+          setQuotes(response.data.content)
+        })
+    }
+    fetchQuotes()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="quotes-div">
+        <p>{quotes}</p>
+        <img src={refresh} alt='refresh icon' className='refresh-icon' />
+      </div>
+      <h2>{quotes}</h2>
+      {new Date(time).toLocaleString()}
     </div>
   );
 }
